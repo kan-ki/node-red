028c099482db43095c7a287c022c61fa76ee3db1
kan-ki <33562112+kan-ki@users.noreply.github.com>
2017-11-19 18:18:33 +0900

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -44,7 +44,7 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        browserName: 'chrome',
+        browserName: 'phantomjs',
         chromeOptions: {
         //  args: ['--headless', '--disable-gpu'],
         },
@@ -114,7 +114,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    // services: ['selenium-standalone'],
+    services: ['phantomjs'],
     //
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
